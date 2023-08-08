5de1a740ead76de47ce8e0433b1025301f121229
Алексей Демиденко <kuberg8@gmail.com>
2023-08-08 11:19:20 +0500

    add github url

--- src/router.js
+++ src/router.js
@@ -1,32 +1,32 @@
-import { createWebHistory, createRouter } from "vue-router";
-import Catalog from "@/views/catalog/Catalog.vue";
-import CatalogDetail from "@/views/catalog/_id.vue";
-import Converter from "@/views/Converter.vue";
+import { createWebHistory, createRouter } from 'vue-router'
+import Catalog from '@/views/catalog/Catalog.vue'
+import CatalogDetail from '@/views/catalog/_id.vue'
+import Converter from '@/views/Converter.vue'
 
 const routes = [
   // catalog
   {
-    path: "/catalog",
-    name: "Catalog",
+    path: '/catalog',
+    name: 'Catalog',
     component: Catalog,
   },
   {
-    path: "/catalog/:id",
-    name: "CatalogDetail",
+    path: '/catalog/:id',
+    name: 'CatalogDetail',
     component: CatalogDetail,
   },
 
   // converter
   {
-    path: "/converter",
-    name: "Converter",
+    path: '/converter',
+    name: 'Converter',
     component: Converter,
   },
-];
+]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/usd-vue3/'), // or createWebHistory(process.env.BASE_URL)
   routes,
-});
+})
 
-export default router;
+export default router
