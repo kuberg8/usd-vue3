26af9af5441b1638650121b06fe086ef36c8fb3b
Алексей Демиденко <kuberg8@gmail.com>
2023-10-10 00:57:11 +0300

    added custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/usd-vue3/'
-      : '/'
-  }
+  // publicPath: process.env.NODE_ENV === 'production' ? '/usd-vue3/' : '/',
+}
