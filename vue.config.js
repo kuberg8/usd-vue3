6744891c57c779de86e939705566ff25e6f49742
Алексей Демиденко <kuberg8@gmail.com>
2023-10-10 23:03:26 +0300

    change vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   // publicPath: process.env.NODE_ENV === 'production' ? '/usd-vue3/' : '/',
+
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    },
+    404: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: '404.html',
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    },
+  },
 }
