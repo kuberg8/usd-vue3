f8286f96154e6ded58f6cb0bec695d5e6709cd49
Алексей Демиденко <kuberg8@gmail.com>
2023-10-10 20:01:34 +0300

    for github custom domain

--- src/router.js
+++ src/router.js
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/usd-vue3/'), // or createWebHistory(process.env.BASE_URL)
+  history: createWebHistory(),// createWebHistory('/usd-vue3/') or createWebHistory(process.env.BASE_URL)
   routes,
 })
 
