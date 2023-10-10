245b63c943d630178d24fe5ae367f7710c871758
Алексей Демиденко <kuberg8@gmail.com>
2023-10-10 20:16:19 +0300

    adding dist subtree

--- src/utils/utils.js
+++
@@ -1,5 +0,0 @@
-export const sym = (currency, locale = "ru") =>
-  new Intl.NumberFormat(locale, { style: "currency", currency }).formatToParts(1).find((x) => x.type === "currency")
-    .value;
-
-export const currencyFormat = new Intl.NumberFormat();
